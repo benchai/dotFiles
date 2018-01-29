ae9715c99363e95b50a014530970a63720ed85ef
Ben Chai <chai@disco.com>
2018-01-29 14:33:53 -0600

    git branch in fish

--- config.fish
+++ config.fish
@@ -1,1 +1,25 @@
 set -x PATH ~/Library/Python/2.7/bin $PATH
+
+set fish_git_dirty_color red
+set fish_git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_status (git status -s)
+
+  if test -n "$git_status"
+    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+  else
+    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+function fish_prompt
+  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
+  if test -n "$git_dir"
+    printf '%s@%s %s%s%s:%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
+  else
+    printf '%s@%s %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  end
+end
+
